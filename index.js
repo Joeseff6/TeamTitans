6702e2ed298d7d6e98765c0534a46d531428ae6c
Joseph Soria <josephsoria6@gmail.com>
2021-02-10 23:45:26 -0600

    Imported bootstrap module

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const Employee = require(`./lib/Employee`);
 const Intern = require(`./lib/Intern`);
 const Manager = require(`./lib/Manager`);
 const Engineer = require(`./lib/Engineer`);
+const bootstrap = require(`bootstrap`);
