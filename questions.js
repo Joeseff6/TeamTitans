a90eaa4f2fd8ba79194d4d2ae7ee86fbe26e17fd
Joseph Soria <josephsoria6@gmail.com>
2021-02-11 23:07:06 -0600

    Changed questions into an object for asking different questions

---
+++ questions.js
@@ -0,0 +1,56 @@
+const roles = [`Manager`, `Engineer`, `Intern`]
+
+const questions = {
+    startingQuestions: [
+        {
+            type: `input`,
+            name: `name`,
+            message: `What is the employee name?`,
+        },
+        {
+            type: `input`,
+            name: `id`,
+            message: `What is the employee's id number?`,
+        },
+        {
+            type: `input`,
+            name: `email`,
+            message: `What is the employee's email address?`,
+        },
+        {
+            type: `list`,
+            name: `role`,
+            message: `What is the employee's role?`,
+            choices: roles,
+        },
+    ],
+
+    managerQuestions: [
+        {
+            type: `input`,
+            name: `office`,
+            message: `What is the manager's office number?`
+        },
+    ],
+
+    engineerQuestions: [
+        {
+            type: `input`,
+            name: `office`,
+            message: `What is the manager's office number?`
+        },
+    ],
+
+    internQuestions: [
+        {
+            type: `input`,
+            name: `office`,
+            message: `What is the manager's office number?`
+        },
+    ],
+};
+
+
+
+
+module.exports = questions;
